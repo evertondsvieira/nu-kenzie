17c1a9016fb08ffd3f5163c8fae53b6324b1df52
Everton Vieira <vieiraeverton33@hotmail.com>
2023-03-27 00:28:41 -0300

    Feat: create app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "./reset.css"
+
+import React from 'react'
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import Form from "./components/Form"
+import Header from "./components/Header"
+import List from "./components/List"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listTransactions, setListTransactions] = useState([])
+
+  const [formData, setFormData] = useState({
+    description: "",
+    type: "Entrada",
+    value: ""
+  })
+
+  const total = listTransactions.reduce((valorAnterior, valorAtual) => {
+    return valorAtual.value + valorAnterior;
+  }, 0)
+
+  const itensFiltrados = (transaction) => {setListTransactions(listTransactions.filter((item) => {
+    return item !== transaction
+  }))}
+
+  const notify = () => toast("Há campos vazios!")
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    if(formData.description === "" || formData.value === ""){
+      notify()
+    } else {
+      const newTransactions = {
+        description: formData.description,
+        type: formData.type,
+        value: formData.type === "Saída" ? Number(formData.value) * (-1) : Number(formData.value)
+      }
+      setListTransactions([...listTransactions, newTransactions])
+    }
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <Header />
+      <div className="pai">
+        <ToastContainer />
+        <Form 
+          formData={formData}
+          listTansactions={listTransactions}
+          setListTransactions={setListTransactions}
+          setFormData={setFormData}
+          total={total}
+          handleSubmit={handleSubmit}
+        />
+        <List 
+          listTransactions={listTransactions}
+          itensFiltrados={itensFiltrados}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
